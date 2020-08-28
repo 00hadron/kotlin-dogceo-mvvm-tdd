49bb4cbcb85d4d6206686c67d1de9485c71dbc2d
Katya Popova <hadron@yandex.ru>
2020-08-28 18:37:42 +0300

    init

---
+++ app/src/main/java/ru/hadron/kotlin_dogceo_mvvm_tdd/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.hadron.kotlin_dogceo_mvvm_tdd
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
